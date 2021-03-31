4a9bd685afa870fcd6327622cc0ef8a1cca6b77a
Oscarbk <oscarbk@gwu.edu>
2021-03-30 20:15:09 -0500

    added functionality to any switch in home fragment

--- RestaurantRecommender/app/src/main/java/com/example/restaurantrecommender/ui/login/ui/home/HomeFragment.kt
+++ RestaurantRecommender/app/src/main/java/com/example/restaurantrecommender/ui/login/ui/home/HomeFragment.kt
@@ -1,18 +1,23 @@
 package com.example.restaurantrecommender.ui.login.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.restaurantrecommender.R
+import com.google.android.material.chip.ChipGroup
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var anyCuisine: SwitchCompat
+    private lateinit var chipGroup: ChipGroup
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +31,15 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        anyCuisine = root.findViewById(R.id.anySwitch)
+        chipGroup = root.findViewById(R.id.chipGroup)
+
+        anyCuisine.setOnCheckedChangeListener { buttonView, _ ->
+            if (buttonView.isChecked) chipGroup.visibility = View.GONE
+            else                      chipGroup.visibility = View.VISIBLE
+        }
+
         return root
     }
 }
